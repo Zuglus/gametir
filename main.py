c4be4efb528f51e76b1f2d8ce0784a8c0475db99
Zuglus <kynterets@gmail.com>
2024-09-19 09:29:03 +0400

    добавлены параметры экрана

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+from turtledemo.nim import SCREENWIDTH
+
 import pygame
 
 pygame.init()
-running = True
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Игра Тир")
+icon = pygame.image.load()
+
+running = True
 while running:
     pass
 
